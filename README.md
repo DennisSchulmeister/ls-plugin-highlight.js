88bdbde25efa76af68be2e71bac18aa7a14f2ea1
Dennis Schulmeister-Zimolong <dennis@patk.org>
2025-03-16 16:44:43 +0100

    [v 2.1.0] Highlight lines

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 lecture-slides.js: Syntax Highlighting Plugin
 =============================================
 
+1. [Description](#description)
+1. [Installation](#installation)
+1. [Simple Usage](#simple-usage)
+1. [Highlighting Lines](#highlighting-lines)
+1. [Copyright](#copyright)
+
+
 Description
 -----------
 
@@ -8,13 +15,12 @@ This plugin adds the ability to have source code examples with syntax highlighti
 to `@dschulmeis/lecture-slides.js` or `@dschulmeis/mini-tutorial.js`. Thanks to
 `highlight.js` examples can use almost any programming language without much effort.
 
-![Screenshot](screenshot.png)
+![Screenshot](.screenshots/example.png)
 
 Installation
 ------------
 
- 1. Add the plugin to your project:
-    `$ npm add --save-dev @dschulmeis/ls-plugin-highlight.js`
+ 1. Add the plugin to your project: `$ npm add --save-dev @dschulmeis/ls-plugin-highlight.js`
  2. Import it in the `index.js` file
  3. Import the needed [languages that come with highlight.js](https://highlightjs.org/static/demo/)
  4. Use the HTML tags below in your presentation
@@ -81,8 +87,8 @@ the following properties:
  * `highlightAll`: Automatically highlight all `<pre><code>` blocks found
    in the HTML document. Default: `false`
 
-Usage
------
+Simple Usage
+------------
 
   * `<source-code language="...">` for complete code blocks
   * `<src-code language="...">` for inline code snippets
@@ -129,11 +135,60 @@ usual for `highlight.js`:
 </pre>
 ```
 
+Highlighting Lines
+------------------
+
+For most cases, the above HTML elements are all you need to include some syntax-highlighted
+code in your presentations. But sometimes you might want to bring out some lines for further
+explanation. This can be done with the `highlight` attribute and the `highlightLines` method.
+
+![Example of highlighted lines](.screenshots/highlighted-lines.png)
+
+To staticaly highlight some lines, pass a comma-separated list of line numbers (starting
+at one) or ranges (as two-item arrays) to the `highlight` attribute:
+
+```html
+<source-code language="javascript" highlight="1, [9, 17]">
+    ...
+</source-code>
+```
+
+To change the highlighted lines at runtime, pass an `id` and call the `highlightLines` method
+likewise:
+
+```html
+<source-code language="javascript" id="example">
+    ...
+</source-code>
+
+<script>
+    const element = document.getElementById("example");
+    element.highlightLines([1, [9, 17]]);
+</script>
+```
+
+Below is a somewhat contrieved example, that always highlights the next line every second.
+Notice the `lines` property which stores the number of lines of the source code.
+
+```javascript
+window._line = 0;
+
+window.setInterval(() => {
+    let element = document.getElementById("example");
+    if (!element) return;
+
+    window._line++;
+    if (window._line > element.lines) window._line = 1;
+
+    element.highlightLines(window._line);
+}, 1000);
+```
+
 Copyright
 ---------
 
 lecture-slides.js: https://www.github.com/DennisSchulmeister/lecture-slides.js <br/>
 mini-tutorial.js: https://www.github.com/DennisSchulmeister/mini-tutorial.js <br/>
 ls-plugin-highlight.js: https://github.com/DennisSchulmeister/ls-plugin-highlight.js <br/>
-© 2017 – 2022 Dennis Schulmeister-Zimolong <dennis@pingu-mail.de> <br/>
+© 2017 – 2025 Dennis Schulmeister-Zimolong <dennis@pingu-mail.de> <br/>
 Licensed under the 2-Clause BSD License.
--- index.js
+++ index.js
@@ -1,43 +1,19 @@
 /*
  * ls-plugin-highlight.js (https://www.wpvs.de)
- * © 2017 – 2022  Dennis Schulmeister-Zimolong <dennis@pingu-mail.de>
+ * © 2017 – 2025  Dennis Schulmeister-Zimolong <dennis@pingu-mail.de>
  * License of this file: BSD 2-clause
  */
-"use strict";
-
-import hljs from 'highlight.js/lib/core';
+import hljs                            from "highlight.js/lib/core";
+import { determineLinebreaks }         from "@dschulmeis/ls-utils/string_utils.js";
 import { removeSurroundingWhitespace } from "@dschulmeis/ls-utils/string_utils.js";
+import { copyAttributes }              from "@dschulmeis/ls-utils/dom_utils.js";
 import "./style.css";
 
 /**
  * This is a simple HTML plugin for `@dschulmeis/lecture-slides.js` and
  * `@dschulmeis/mini-tutorial.js`. It adds two new HTML tags to create syntax
- * highlighted code examples with highlight.js. Usage:
- *
- *   * `<source-code language="...">` for complete code blocks
- *   * `<src-code language="...">` for inline code snippets
- *
- * The constructor takes an optional configuration object that can be used
- * to define the supported languages and change the highlight.js settings:
- *
- *   ```javascript
- *   import HLJS_Language_XML from 'highlight.js/lib/languages/xml';
- *   import "highlight.js/styles/atom-one-light.css";
- *
- *   let plugin = new LS_Plugin_HighlightJS({
- *       // Supported languages
- *       languages: {
- *           "xml": HLJS_Language_XML,
- *       },
- *
- *       // Highlight.js configuration, see highlight.js method configure()
- *       highlightjs: {
- *       },
- *
- *       // Automatically highlight all <pre><code> elemenets (default: false)
- *       highlightAll: true,
- *   });
- *   ```
+ * highlighted code examples with highlight.js. See [README.md](README.md)
+ * for usage details.
  *
  * Please note, that by default no languages are available unless they are
  * explicitly imported like shown above. This is intentional to keep the
@@ -51,8 +27,8 @@ export default class LS_Plugin_HighlightJS {
     constructor(config) {
         // Interpret configuration values
         this._config = config || {};
-        this._config.languages = this._config.languages || {};
-        this._config.highlightJs = this._config.highlightJs || {ignoreUnescapedHTML: true};
+        this._config.languages    = this._config.languages || {};
+        this._config.highlightJs  = this._config.highlightJs || {ignoreUnescapedHTML: true};
         this._config.highlightAll = this._config.highlightAll || false;
 
         // Configure highlight.js and register languages
@@ -90,16 +66,17 @@ export default class LS_Plugin_HighlightJS {
 
         for (let element of sourceCodeElements) {
             try {
-                let language = element.getAttribute("language") || "";
+                let language  = element.getAttribute("language") || "";
                 let classname = language ? `language-${language}` : "";
 
-                let code = removeSurroundingWhitespace(element.innerHTML);
-                let newElement = null;
+                let code        = removeSurroundingWhitespace(element.innerHTML);
+                let newElement  = null;
                 let codeElement = null;
 
                 if (element.nodeName === "SOURCE-CODE") {
                     // Block level element
                     newElement = document.createElement("pre");
+                    copyAttributes(element, newElement);
                     newElement.classList.add("code");
 
                     codeElement = document.createElement("code");
@@ -110,15 +87,92 @@ export default class LS_Plugin_HighlightJS {
                 } else if (element.nodeName === "SRC-CODE") {
                     // Inline element
                     newElement = codeElement = document.createElement("code");
+                    copyAttributes(element, newElement);
                     if (classname) codeElement.classList.add(classname);
                     codeElement.innerHTML = code;
                 }
 
                 hljs.highlightElement(codeElement);
                 element.replaceWith(newElement);
+
+                if (element.nodeName === "SOURCE-CODE") {
+                    this._mixinHighlightedLines(newElement);
+                }
             } catch(error) {
                 console.warn("@dschulmeis/ls-plugin-highlight.js:", error);
             }
+        }
+    }
+
+    /**
+     * Add functionality to highlight single lines or line ranges, to be able to better
+     * explain individual code blocks. This method interprets the HTML attribute `highlight`,
+     * which may contain a comma-separated list of line numbers and line ranges, e.g.:
+     * 
+     * `1, 3, [7, 9]`
+     * 
+     * This would highlight the lines 1, 3, and 7 to 9. Additionaly, a method called 
+     * `highlightLines` is added to the HTML element, which takes a single array of the
+     * same format to change the highlighting at runtime.
+     * 
+     * @param{Element} newElement New HTML element
+     */
+    _mixinHighlightedLines(newElement) {
+        // Wrap all source-lines with <span class="hljs-line"></span> to be able
+        // to highlight them, if needed
+        let codeElement = newElement.querySelector("code");
+        let innerHTML   = codeElement.innerHTML;
+        let linebreak   = determineLinebreaks(innerHTML);
+        let lines       = linebreak ? innerHTML.split(linebreak) : [innerHTML];
+        lines = lines.slice(0, -1);
+
+        for (let i in lines) {
+            lines[i] = `<span class="hljs-line">${lines[i] || " "}</span>`;
+        }
+
+        codeElement.innerHTML = lines.join(linebreak || "\n");
+
+        // Add method to highlight lines via JavaScript
+        newElement.highlightLines = highlightLines.bind(newElement);
+
+        // Add `lines` property with the number of lines
+        newElement.lines = lines.length;
+
+        // Apply highlighed lines, if the highligt attribute is set
+        let highlight = newElement.getAttribute("highlight");
+        if (highlight) newElement.highlightLines(JSON.parse(`[${highlight}]`));
+    }
+}
+
+/**
+ * This function will be added as a bound method to the new HTML element, to
+ * allow highlighting lines at runtime. In JavaScript the source code element
+ * can be found e.g. via its ID and then this method can be called, to change
+ * the currently highlighted lines.
+ * 
+ * The array may contain single line numbers (as numbers) and line ranges (
+ * as nested arrays). To clear the highlighting simply call this method with
+ * a missing or empty array.
+ * 
+ * @param {Array} highlight Line numbers and ranges to highlight
+ */
+function highlightLines(highlights) {
+    let lineElements = this.querySelectorAll(".hljs-line");
+
+    for (let lineElement of lineElements) {
+        lineElement.classList.remove("hljs-highlighted");
+    }
+
+    if (!Array.isArray(highlights)) highlights = [highlights];
+
+    for (let highlight of highlights || []) {
+        let range = Array.isArray(highlight) ? highlight : [highlight, highlight];
+        if (range.length < 2) range = [range[0], range[0]];
+
+        for (let i = range[0]; i <= range[1]; i++) {
+            let lineElement = lineElements[i - 1];
+            if (!lineElement) break;
+            lineElement.classList.add("hljs-highlighted");
         }
     }
 }
